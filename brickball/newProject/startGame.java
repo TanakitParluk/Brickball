2f5fc096c6cc1ce98a96a4b1ccb02d7e591641be
TanakitParluk <74202248+TanakitParluk@users.noreply.github.com>
2020-11-13 15:46:52 +0700

    Add files via upload

---
+++ brickball/newProject/startGame.java
@@ -0,0 +1,32 @@
+package newProject;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class startGame extends JFrame{
+	screenGame screen = new screenGame();
+	
+	public startGame() {
+		add(screen);
+		setTitle("Brick crusher!!!");
+		setSize(700,600);
+		setVisible(true);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		new startGame();
+	}
+	
+	
+
+}
